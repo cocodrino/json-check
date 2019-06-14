2d0d973e05e2f167ac05039ce2adee2d2b53436f
Carlos LM <clagccs@gmail.com>
2019-06-14 15:54:43 -0400

    first implementation

---
+++ README.md
@@ -0,0 +1,12 @@
+## CHALLENGE
+
+the frontend code is inside client folder
+
+run with
+
+```
+npm install
+npm run start
+```
+
+open the url localhost:5000 
---
+++ public/precache-manifest.7da1baffb5d9997f097a7c249dd6e78a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aedd6c44fe338dd84f600e6754eed79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c9b807cb2c4c5d51cb5",
+    "url": "/static/css/main.a74f6eb0.chunk.css"
+  },
+  {
+    "revision": "37f43443d90a809f11cb",
+    "url": "/static/js/2.e5a2dc60.chunk.js"
+  },
+  {
+    "revision": "5c9b807cb2c4c5d51cb5",
+    "url": "/static/js/main.0fb5a281.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
